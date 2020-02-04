4283d49f5cf16546d52e983c5942f52ad8ab7388
cadcad-sat <pr.kadooka@gmail.com>
2020-02-04 17:41:08 +0900

    テスト

---
+++ chapter001/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,worlds");
+        }
+    }
+}
